db694bf014f9e7ecb84a42c64784c394f7ce009d
TK <zeropercent@hotmail.com>
2019-03-05 21:00:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object, {[key]: value})
 }
 
-
+function deleteFromObjectByKey(object, key){
+    var newObj = Object.assign({}, object)
+    delete newObj.[key]
+    return newObj
+}
 
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key]
